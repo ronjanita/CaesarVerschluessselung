42e1b83513ffac78eeb6937c76b1b81621dc42b1
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-02-24 15:57:39 +0100

    upload 2

--- CaesarVerschluesselung/obj/Debug/net8.0/CaesarVerschluesselung.AssemblyInfo.cs
+++ CaesarVerschluesselung/obj/Debug/net8.0/CaesarVerschluesselung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaesarVerschluesselung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad979d47e0c264cc7c850bcab003ecf273d8251")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaesarVerschluesselung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaesarVerschluesselung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
