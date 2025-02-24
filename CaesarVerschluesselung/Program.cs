0ad979d47e0c264cc7c850bcab003ecf273d8251
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-02-24 15:47:59 +0100

    upload

---
+++ CaesarVerschluesselung/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Caesar
+{
+    static string Encrypt(string text, int shift)
+    {
+        string result = "";
+        foreach (char letter in text)
+        {
+            if (char.IsLetter(letter))
+            {
+                char baseChar = char.IsUpper(letter) ? 'A' : 'a';
+                int newPosition = letter - baseChar + shift;
+                if (newPosition > 25)
+                {
+                    newPosition -= 26;
+                }
+                else if (newPosition < 0)
+                {
+                    newPosition += 26;
+                }
+                result += (char)(baseChar + newPosition);
+            }
+            else
+            {
+                result += letter;
+            }
+        }
+        return result;
+    }
+}
+
